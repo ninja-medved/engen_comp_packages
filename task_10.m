a3f2520a2c01097949ece7c0e5ffcbd6f6e55704
ninja-medved <88436826+ninja-medved@users.noreply.github.com>
2022-06-05 00:49:05 +0300

    Delete task_10.m

--- task_10.m
+++
@@ -1,9 +0,0 @@
-a = input("Enter value of a:");
-b = input("Enter value of b:");
-phi_0 = input("Enter value of phi_0:");
-n = input("Enter value of n:");
-m = input("Enter value of m:");
-phi = 0:0.05:2*pi;
-x = a * sin(n * phi + phi_0);
-y = b * sin(m * phi);
-plot(x,y)
